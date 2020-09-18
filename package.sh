3ef9b082356f8744356827afc47f00fe2a0c85c6
sebagomez <sebastiangomezcorrea@gmail.com>
2020-09-18 17:24:12 -0300

    Removed unneeded file

--- package.sh
+++
@@ -1,3 +0,0 @@
-dotnet build --configuration Release src/k8s-controller-sdk/k8s-controller-sdk.csproj
-
-dotnet pack --configuration Release --no-build --output ./bin src/k8s-controller-sdk/k8s-controller-sdk.csproj
