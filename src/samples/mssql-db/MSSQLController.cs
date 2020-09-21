c6a98106f5bf87d9ca85806983d67de500eae603
sebagomez <sebastiangomezcorrea@gmail.com>
2020-09-20 23:11:09 -0300

    Little more logging added. Log4Net should be the way

--- src/samples/mssql-db/MSSQLController.cs
+++ src/samples/mssql-db/MSSQLController.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using K8sControllerSDK;
 
 namespace mssql_db
@@ -7,12 +8,18 @@ namespace mssql_db
 	{
 		static void Main(string[] args)
 		{
+			Console.WriteLine($"=== {nameof(MSSQLController)} STARTING ===");
+
 			MSSQLDBOperationHandler handler = new MSSQLDBOperationHandler();
 			Controller<MSSQLDB> controller = new Controller<MSSQLDB>(new MSSQLDB(), handler);
 			Task t1 = controller.SatrtAsync(new System.Threading.CancellationToken());
 			Task t2 = handler.CheckCurrentState(controller.Kubernetes);
 
+			Console.WriteLine($"=== {nameof(MSSQLController)} STARTED ===");
+
 			Task.WaitAll(new Task[] { t1, t2 });
+
+			Console.WriteLine($"=== {nameof(MSSQLController)} TERMINATING ===");
 		}
 	}
 }
