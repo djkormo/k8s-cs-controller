91d94b37b3630f9a8c7b1101e4a62886296079f5
Sebastian Gomez <sebastiangomezcorrea@gmail.com>
2020-10-04 12:40:35 -0300

    The Reconciliation loop is now called by the controller framework itself (on startAsync), and does need to be called by the controller The check interval is now configurable There's a new bash script that returns the version of the framework. Not sure when/if I'll use it but it looks interesting

--- src/BaseCRD.cs
+++ src/BaseCRD.cs
@@ -1,18 +1,21 @@
 ﻿using k8s;
 using k8s.Models;
+using NLog;
 
 namespace K8sControllerSDK
 {
 	public abstract class BaseCRD : IMetadata<V1ObjectMeta>
 	{
-		protected BaseCRD(string group, string version, string plural, string singular)
+		protected BaseCRD(string group, string version, string plural, string singular, int reconInterval = 5)
 		{
 			Group = group;
 			Version = version;
 			Plural = plural;
 			Singular = singular;
+			ReconciliationCheckInterval = reconInterval;
 		}
 
+		public int ReconciliationCheckInterval { get; protected set; }
 		public string Group { get; protected set; }
 		public string Version { get; protected set; }
 		public string Plural { get; protected set; }
